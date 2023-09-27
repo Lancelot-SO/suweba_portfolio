42f295f8cc15f9c03f09f1aed07daeec8a7e44f5
Lancelot-SO <fsowah001@gmail.com>
2023-09-27 15:42:56 +0000

    add projects

--- src/data.js
+++ src/data.js
@@ -1,22 +1,33 @@
 import project2 from "./assets/8.jpeg";
 import project1 from "./assets/4.jpeg";
 import project4 from "./assets/10.jpeg";
+import homepage from "./assets/img/Homepage.png";
+import rectangle from "./assets/img/rectangle.png";
+import rectangle1 from "./assets/img/rectangle-1.png";
+import rectangle2 from "./assets/img/rectangle-2.png";
+import rectangle3 from "./assets/img/rectangle-3.png";
+import rectangle4 from "./assets/img/rectangle-4.png";
+import rectangle5 from "./assets/img/rectangle-5.png";
+
 // upload all the images where and change them in the projectData array
 // by changing the project2, project1, project4 to thename of the image i upload
 
 export const projectsData = [
   {
     id: "1",
-    src: project1,
-    src2: project2,
-    src3: project4,
-    src4: project1,
-    title: "Project 1",
+    src: homepage,
+    src1: rectangle,
+    src2: rectangle1,
+    src3: rectangle2,
+    src4: rectangle3,
+    src5: rectangle4,
+    src6: rectangle5,
+    title: "Teen Finance Tutor",
     description: "Description of Project 1.",
   },
   {
     id: "2",
-    src: project2,
+    src: rectangle1,
     src2: project2,
     src3: project4,
     src4: project1,
@@ -25,7 +36,7 @@ export const projectsData = [
   },
   {
     id: "3",
-    src: project1,
+    src: rectangle2,
     src2: project2,
     src3: project4,
     src4: project1,
@@ -34,16 +45,7 @@ export const projectsData = [
   },
   {
     id: "4",
-    src: project4,
-    src2: project2,
-    src3: project4,
-    src4: project1,
-    title: "Project 4",
-    description: "Description of Project 4.",
-  },
-  {
-    id: "4",
-    src: project4,
+    src: rectangle3,
     src2: project2,
     src3: project4,
     src4: project1,
@@ -52,7 +54,16 @@ export const projectsData = [
   },
   {
     id: "5",
-    src: project4,
+    src: rectangle4,
+    src2: project2,
+    src3: project4,
+    src4: project1,
+    title: "Project 4",
+    description: "Description of Project 4.",
+  },
+  {
+    id: "6",
+    src: rectangle5,
     src2: project2,
     src3: project4,
     src4: project1,
